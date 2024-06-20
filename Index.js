48d53fcbb22e0302baf499cd0b0d9e5e3890d029
vihang02 <vihangamuthumala@gmail.com>
2024-06-20 21:42:57 +0530

    Refactor code to use path module for file operations

--- Index.js
+++ Index.js
@@ -1,2 +1,17 @@
-require('./Batman');
-require('./Superman');
+const path = require('node:path')
+
+console.log(path.basename(__filename));
+console.log(path.basename(__dirname));
+
+console.log(path.extname(__filename));
+console.log(path.extname(__dirname));
+
+console.log(path.parse(__filename));
+console.log(path.format(path.parse(__dirname)));
+
+console.log(path.isAbsolute(__filename));
+console.log(path.isAbsolute(__dirname));
+
+console.log(path.join('folder1', 'folder2', 'index.html'));
+
+console.log(path.resolve('folder1', 'folder2', 'index.html'));
---
+++ main.mjs
@@ -0,0 +1,3 @@
+import add from './math-esm.mjs';
+
+console.log(add(1, 2)); 
