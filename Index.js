ba93850f45d7dae94d51496092ca56f4ab0ff114
vihang02 <vihangamuthumala@gmail.com>
2024-06-20 19:57:03 +0530

    Refactor code to use separate module for addition

--- Index.js
+++ Index.js
@@ -1,7 +1,2 @@
-const add = require("./Add")
-
-console.log('hello');
-const sum = add(1,2);
-const sum2 = add(2,3);
-console.log(sum);
-console.log(sum2);
+require('./Batman');
+require('./Superman');
