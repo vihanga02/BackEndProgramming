a73fb23b82b18d55229ebce7ce244e69b3a39882
vihang02 <vihangamuthumala@gmail.com>
2024-06-21 09:56:32 +0530

    Refactor code to use fs/promises module for file operations

--- Index.js
+++ Index.js
@@ -1,2 +1,16 @@
-const fs = require("node:fs");
+const fs = require("node:fs/promises");
 
+console.log('First');
+
+async  function readFiles() {
+    try {
+        const data = await fs.readFile('file1.txt', 'utf-8');
+        console.log(data);
+    } catch (error) {
+        console.error('Error reading the file', error);
+    }
+}
+
+readFiles();
+
+console.log('Last');
