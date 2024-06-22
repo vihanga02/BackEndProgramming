59c97da5a4724b914c4c5930ce8fc0679153f580
vihang02 <vihangamuthumala@gmail.com>
2024-06-22 09:22:05 +0530

    chore: Refactor server code to handle different routes and return appropriate responses

--- Index.js
+++ Index.js
@@ -2,11 +2,27 @@ const http = require('node:http');
 const fs = require('node:fs');
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    const html = fs.readFileSync("./index.html");
-    res.end(html)
+    if (req.url === '/'){
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end("Home Page");
+    }
+    else if (req.url === '/about'){
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end("About Page");
+    }
+    else if (req.url === '/api'){
+        res.writeHead(200, {"Content-Type": "application/json"});
+        res.end(JSON.stringify({
+            name: "John Doe",
+            age: 25,
+        }))
+    }
+    else{
+        res.writeHead(404);
+        res.end("Page Not Found")
+    }
 });
 
 server.listen(3000, () => {
-    console.log("Server is running on port 3000")
+    console.log("Server is running on port 3000");
 })
